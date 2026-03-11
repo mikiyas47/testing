61f90974c4aac220875eb47e3fb25c8dac615890
Mikiyas Shemels <mikishemels@gmail.com>
2026-03-11 11:29:44 +0300

    Add endpoint to view all registered users

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Models\User;
 
+// Existing routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+// Admin testing route to view all users
+Route::get('/all-users', function () {
+    return response()->json(User::all());
+});
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
